4a5aec45dbabb9927c90bfb7a26bdc50e4a52fd8
Ryan Cramer <ryan@rc-d.net>
2017-03-14 14:24:35 -0400

    Some CSS tweaks, plus a couple new config options

--- _offcanvas.php
+++ _offcanvas.php
@@ -15,16 +15,6 @@ if(!defined("PROCESSWIRE")) die();
 <!-- OFFCANVAS NAVIGATION -->
 <div id="offcanvas-nav" uk-offcanvas>
 	<div class="uk-offcanvas-bar">
-			<!--
-		<form class='uk-margin' action='<?php echo $urls->admin; ?>page/search/' method='get'> 
-			<div class="uk-inline">
-				<span class="uk-form-icon"><?php echo $adminTheme->renderIcon('search'); ?></span>
-				<input type='text' class='uk-input uk-form-width-large' placeholder='' name='q' value='' />
-			</div>
-			<input class='uk-hidden' type='submit' name='search' value='Search' />
-			<input type='hidden' name='show_options' value='1' />
-		</form>
-		-->
 		<?php include(__DIR__ . '/_search-form.php'); ?>
 		<ul class='pw-sidebar-nav uk-nav uk-nav-primary uk-nav-parent-icon' data-uk-nav='animation: false; multiple: true;'>
 			<?php echo $adminTheme->renderSidebarNavItems(); ?>
